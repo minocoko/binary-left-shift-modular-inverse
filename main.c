74005df1aa88a982c991ad058fd677cffd6185c9
Minco Ke <minco_ke@epam.com>
2021-09-12 02:01:52 +0800

    reduce unnecessary loop times

--- main.c
+++ main.c
@@ -7,14 +7,18 @@
 unsigned int inv_mod(const unsigned int a, const unsigned int m)
 {
     // How often does this really happen? it should "almost" never happen (but would be faster)
-    // if (a == 1)
-    // {
-    //     return a;
-    // }
-    // if (a == 2)
-    // {
-    //     return (m + 1)  >> 1;
-    // }
+    if (a == 1)
+    {
+        return 1;
+    }
+    if (a == 2)
+    {
+        return (m + 1) >> 1;
+    }
+    if (a == m - 1)
+    {
+        return a;
+    }
 
     // check most significant bit
     const int u_msb = 32 - __builtin_clz(m);
@@ -34,20 +38,6 @@ unsigned int inv_mod(const unsigned int a, const unsigned int m)
     long r = 0;
     long s = 1;
 
-    // reduce unnecessary loop times
-    if (msb_diff > 0)
-    {
-        cu = 1;
-        two_pow_of_cu = 1 << 1;
-        cv = msb_diff + 1;
-        two_pow_of_cv = 1L << cv;
-
-        u <<= 1;
-        v <<= cv;
-
-        s = 1L << msb_diff;
-    }
-
     do
     {
         if (abs_u < un1)
@@ -64,7 +54,8 @@ unsigned int inv_mod(const unsigned int a, const unsigned int m)
             cu++;
             two_pow_of_cu <<= 1;
         }
-        else if (abs_v < un1)
+
+        if (abs_v < un1)
         {
             v <<= 1;
             if (cv >= cu)
@@ -78,42 +69,36 @@ unsigned int inv_mod(const unsigned int a, const unsigned int m)
             cv++;
             two_pow_of_cv <<= 1;
         }
-        else
+
+        if ((u >= 0 && v >= 0) || (u < 0 && v < 0))
         {
-            if ((u >= 0 && v >= 0) || (u < 0 && v < 0))
+            // op -
+            if (cu <= cv)
             {
-                // op -
-                if (cu <= cv)
-                {
-                    u = u - v;
-                    r = r - s;
-                }
-                else
-                {
-                    v = v - u;
-                    s = s - r;
-                }
+                u = u - v;
+                r = r - s;
             }
             else
             {
-                // op +
-                if (cu <= cv)
-                {
-                    u = u + v;
-                    r = r + s;
-                }
-                else
-                {
-                    v = v + u;
-                    s = s + r;
-                }
+                v = v - u;
+                s = s - r;
+            }
+        }
+        else
+        {
+            // op +
+            if (cu <= cv)
+            {
+                u = u + v;
+                r = r + s;
+            }
+            else
+            {
+                v = v + u;
+                s = s + r;
             }
         }
 
-        if (u == 0 || v == 0)
-        {
-            return 0;
-        }
 
         abs_u = ABS(u);
         abs_v = ABS(v);
@@ -140,13 +125,14 @@ unsigned int inv_mod(const unsigned int a, const unsigned int m)
 
 int main()
 {
+    // const unsigned int a = 1, m = 13;                        // output 1 (1 * 1 % 13 == 1)
     // const unsigned int a = 10, m = 13;                       // output 4 (4 * 10 % 13 == 1)
     // const unsigned int a = 3, m = 997;                       // output 665 (665 * 3 % 997 == 1)
     // const unsigned int a = 2456, m = 5179;                   // output 2541 (2541 * 2456 % 5179 == 1)
     // const unsigned int a = 10, m = 49993;                    // output 14998 (14998 * 10 % 49993 == 1)
     const unsigned int a = 2397485, m = 305175781;              // output 197958680 (197958680 * 2397485 % 305175781 == 1)
     // const unsigned int a = 2, m = 2521008887u;               // output 1260504444 (1260504444 * 2 % 2521008887 == 1)
-    // const unsigned int a = 65536, m = 2521008887u;           // output 2487746851 (2487746851 * 2147483648 % 2521008887 == 1)
+    // const unsigned int a = 65536, m = 2521008887u;           // output 1666452423 (1666452423 * 65536 % 2521008887 == 1)
     // const unsigned int a = 2147483647u, m = 2521008887u;     // output 2069798517 (2069798517 * 2147483647 % 2521008887 == 1)
     // const unsigned int a = 2147483648u, m = 2521008887u;     // output 2487746851 (2487746851 * 2147483648 % 2521008887 == 1)
     // const unsigned int a = 2147483649u, m = 2521008887u;     // output 2058376504 (2058376504 * 2147483649 % 2521008887 == 1)
